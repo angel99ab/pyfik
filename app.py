b1c4ebe287e42239bd9674bfe3c5583645a56c19
Angel Andrade <aandradeb99@gmail.com>
2022-05-11 17:39:54 +0200

    Change from pack to grid geometry manager in root window

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from tkinter import Tk
 from custom_notebook import CustomNotebook
 
+
 class App(Tk):
 
     def __init__(self):
@@ -9,7 +10,10 @@ class App(Tk):
         # root window config
         self.geometry("500x400")
         self.resizable(False, False)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
 
+        # widget inside root window
         self.notebook = CustomNotebook(self)
-        self.notebook.pack(expand=True, fill="both")
+        self.notebook.grid(column=0, row=0, sticky="nesw")
     
