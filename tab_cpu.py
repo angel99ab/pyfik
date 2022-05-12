e8c294422ee609dbb413fadc55eaade7730e9885
Angel Andrade <aandradeb99@gmail.com>
2022-05-12 20:20:23 +0200

    Add max and min cpu frequency into tab_cpu

--- tab_cpu.py
+++ tab_cpu.py
@@ -18,10 +18,21 @@ class TabCPU(ttk.Frame):
         label_frame_threads = ttk.LabelFrame(self, text="Threads")
         threads = ttk.Label(label_frame_threads, text=psutil.cpu_count(), foreground="blue")
 
+        label_frame_max_freq = ttk.LabelFrame(self, text="Max frequency")
+        max_freq = ttk.Label(label_frame_max_freq, text=str(self.cpu_freq.max) + " Mhz", foreground="blue")
+
+        label_frame_min_freq = ttk.LabelFrame(self, text="Min frequency")
+        min_freq = ttk.Label(label_frame_min_freq, text=str(self.cpu_freq.min) + " Mhz", foreground="blue")
+
         brand.pack(expand=1)
         cores.pack(expand=1)
         threads.pack(expand=1)
+        max_freq.pack(expand=1)
+        min_freq.pack(expand=1)
 
         label_frame_brand.place(width=280, height=60, x=10, y=10)
         label_frame_cores.place(width=85, height=60, x=300, y=10)
         label_frame_threads.place(width=85, height=60, x=395, y=10)
+
+        label_frame_max_freq.place(width=230, height=60, x=10, y=80)
+        label_frame_min_freq.place(width=230, height=60, x=250, y=80)
