6f957f226eea6b90641f39dab52ad726e072b422
Angel Andrade <aandradeb99@gmail.com>
2022-05-23 21:20:06 +0200

    Change tab header with blue buttons

--- app.py
+++ app.py
@@ -1,25 +1,63 @@
-from tkinter import Tk
+from doctest import master
+import customtkinter
 from tkinter.ttk import Style
 from custom_notebook import CustomNotebook
 
 
-class App(Tk):
+class App(customtkinter.CTk):
 
-    def __init__(self):
-        super().__init__()
-
-        style = Style()
-        style.theme_use("default")
-        style.configure("TNotebook.Tab", focuscolor="#d6d6d6");
-        style.configure("TButton", focuscolor="#d6d6d6");
-
-        # root window config
+    def __init__(self, *args, fg_color="#ffffff", **kwargs):
+        super().__init__(*args, fg_color=fg_color, **kwargs)
+        
+        # Root window config
+        self.set_appearance_mode("Light")
         self.geometry("500x400")
         self.resizable(False, False)
-        self.grid_rowconfigure(0, weight=1)
+        
+        # Expand the grid cells
         self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=1)
 
-        # widget inside root window
-        self.notebook = CustomNotebook(self)
-        self.notebook.grid(column=0, row=0, sticky="nesw")
-    
+        # Create frames
+        self.header_frame = customtkinter.CTkFrame(master=self, fg_color="#ffffff")
+        self.content_frame = customtkinter.CTkFrame(master=self)
+
+        self.header_frame.grid(row=0, column=0, sticky="we", padx=10)
+        self.content_frame.grid(row=1, column=0, sticky="nesw")
+
+        # Create buttons
+        self.btn_general = customtkinter.CTkButton(master=self.header_frame,
+                                                   text="General",
+                                                   width=30,
+                                                   height=26,
+                                                   text_color="#ffffff")
+
+        self.btn_cpu = customtkinter.CTkButton(master=self.header_frame,
+                                               text="CPU",
+                                               width=30,
+                                               height=26,
+                                               text_color="#ffffff")
+
+        self.btn_memory = customtkinter.CTkButton(master=self.header_frame,
+                                                  text="Memory",
+                                                  width=30,
+                                                  height=26,
+                                                  text_color="#ffffff")
+
+        self.btn_disk_usage = customtkinter.CTkButton(master=self.header_frame,
+                                                  text="Disk usage",
+                                                  width=30,
+                                                  height=26,
+                                                  text_color="#ffffff")
+
+        self.btn_network = customtkinter.CTkButton(master=self.header_frame,
+                                                  text="Network",
+                                                  width=30,
+                                                  height=26,
+                                                  text_color="#ffffff")
+
+        self.btn_general.grid(row=0, column=0)
+        self.btn_cpu.grid(row=0, column=1)
+        self.btn_memory.grid(row=0, column=2)
+        self.btn_disk_usage.grid(row=0, column=3)
+        self.btn_network.grid(row=0, column=4)
